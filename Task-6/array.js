bcf0380e0829d8ae51f0c37f4886414e36a78679
Lugovskoy Dmitriy <kushiy@mail.ru>
2020-03-27 10:24:34 +0300

    Нахождение и удаление элемента из массива

---
+++ Task-6/array.js
@@ -0,0 +1,10 @@
+/*Дан массив['dog', 'cat', 'rabbit', 'mouse'].
+Удалить из массива элемент со значением 'cat' */
+
+let mas = ['dog', 'cat', 'rabbit', 'mouse'];
+for (let i = 0; i < mas.length; i++) {
+    if (mas[i]==='cat') {
+        mas.splice(i, 1);
+    };
+}
+console.log(mas);
