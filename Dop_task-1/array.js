3081fdef035db35acf4e8b3829c30d2e029c9177
Lugovskoy Dmitriy <kushiy@mail.ru>
2020-03-31 10:31:21 +0300

    Дополнительные задачки

---
+++ Dop_task-1/array.js
@@ -0,0 +1,8 @@
+﻿/* В переменной n хранится натуральное трёхзначное число. Вывсети в консоль сумму цифр числа n. */
+let number = prompt('Введите трехзначное число');
+let sum=0;
+console.log('Введенное число - ' + number);
+for (let elem of number) {
+    sum += parseInt(elem);
+}
+console.log('Сумма всех цифр в числе = ' + sum);
---
+++ Dop_task-3/array.js
@@ -0,0 +1,23 @@
+﻿/* Заполните массив на n элементов случайным числами и выведете максимальное, 
+ * минимальное и среднее значение. Размер массива (n) вводит пользователь */
+
+let min=0, max=min, mean, sum=0;
+let size = prompt('Введите размер массива');
+let array = [];
+
+for (let i = 0; i<=size; i++) {
+    array[i] = parseInt(random(0, 100));
+    if (array[i] > max) {
+        max = array[i];
+    } else min = array[i];
+    sum += array[i];
+} 
+mean = sum / size;
+console.log('Произвольный массив ' + array);
+console.log('Минимальное число = ' + min);
+console.log('Максимальное число = ' + max);
+console.log('Среднее число = ' + parseInt(mean));
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+}
