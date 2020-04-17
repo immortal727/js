614e93512699aa45b5bf7d4c02eee344fb425a1d
Lugovskoy Dmitriy <kushiy@mail.ru>
2020-04-17 20:39:15 +0300

    Каждый элемент массива больше нуля

---
+++ function3/function.js
@@ -0,0 +1,20 @@
+﻿'use strict'
+let arr = [
+    [1, 3, 4],
+    [3, 7, 9],
+    [-90, 12, -12]
+];
+
+// каждый элемент вложенного массива больше 0 ???
+console.log(arr);
+let LessZero = (item, index, array) => {
+    for (let i = 0; i < array[index].length; i++) {
+        if (item[i] > 0) {
+            console.log(`Элемент массива [${index}][${i}] = ` + item[i] + ` > 0`)
+            return true;
+        }
+    }
+};
+
+let rez = arr.some(LessZero);
+console.log(rez);
