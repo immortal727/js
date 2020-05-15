305d4fe8cb3a8f881c6594d3f8b470f87357c264
Lugovskoy Dmitriy <kushiy@mail.ru>
2020-05-15 18:20:59 +0300

    Нахождение спам-слова в предложении

---
+++ Spam_word/spamWord.js
@@ -0,0 +1,21 @@
+﻿/* Написать функцию проверки на спам. Функция принимает на вход текст и спам - слова.
+Определить по 5-ти бальной шкале, насколько часто в тексте встречается спам.
+Результат вернуть из функции */
+
+let text = prompt("Введите предложение, которое бы включало спам слова");
+let spam = prompt("Введите спам слово");
+console.log(`${text}`);
+console.log(`Спам-слово ${spam}`);
+
+function spamTest(text, ...spamWord) {
+    let lowerText = text.toLowerCase();
+    textArr = lowerText.split(' '); // загнали строчку в массив
+    let counter = 0;
+    for (let i = 0; i < textArr.length; i++) {
+        spamWord.forEach(function (element) {
+            if (element === textArr[i]) { counter++; }
+        });
+    }
+    console.log(`В тексте встречается ${counter} раз спам-слово ${spamWord}`)
+}
+spamTest(text, spam);
