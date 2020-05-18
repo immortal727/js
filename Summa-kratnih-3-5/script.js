1896e562a8b2c78dbb3c591c37e1f56ad6910440
Lugovskoy Dmitriy <kushiy@mail.ru>
2020-05-18 21:08:16 +0300

    сумму всех чисел кратных 3 или 5

---
+++ Summa-kratnih-3-5/script.js
@@ -0,0 +1,18 @@
+﻿/*Если мы перечислим все натуральные числа до 10, кратные 3 или 5, мы получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+Завершите решение так, чтобы оно возвращало сумму всех чисел кратных 3 или 5.*/
+function solution(number) {
+    let arr = [];
+    for (let i = 1; i < number; i++) {    // генерируем массив от 1 до number
+        arr[i] = i;
+    }
+    return arr;
+}
+//let sum = 0, arr2=[];
+
+let arr = solution(10);
+console.log(arr);
+let result = arr.filter(elem => elem % 3 === 0 || elem % 5 === 0).reduce((sum, elem) => sum + elem, 0);
+/*arr2 = arr.filter((elem, index, array) => (elem % 3 === 0 || elem % 5 === 0));
+arr2.forEach((elem, index, array)=> sum += elem);
+console.log(`Сумма элементов массива ${arr2} = ${sum}`);*/
+console.log(result);
