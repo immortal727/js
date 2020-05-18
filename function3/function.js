0b664abb46730d5ec3785d4c912df112e1ddc5d8
Lugovskoy Dmitriy <kushiy@mail.ru>
2020-05-18 18:16:44 +0300

    Создание нового массива из двумерного, значения которого >0 через стрелочную функцию

--- function3/function.js
+++ function3/function.js
@@ -6,15 +6,23 @@ let arr = [
 ];
 
 // Каждый элемент вложенного массива больше 0 ???
-console.log(arr);
-let LessZero = (item, index, array) => {
+//console.log(arr);
+/*let LessZero = (item, index, array) => {
     for (let i = 0; i < array[index].length; i++) {
         if (item[i] > 0) {
             console.log(`Элемент массива [${index}][${i}] = ` + item[i] + ` > 0`)
             return true;
         }
     }
-};
+};*/
 
-let rez = arr.some(LessZero);
-console.log(rez);
+/*let rez = arr.some(LessZero);
+console.log(rez);*/
+
+//Вывод элементов массива, больше 0
+arr.forEach((elem, index, array) => array[index] = elem.filter((elem, index, array) =>elem > 0));
+
+//Каждый элемент умножается на 2
+//arr.forEach((elem, index, array) => array[index].map((item, index, array) => array[index] *= 2));
+console.log(arr);
+
